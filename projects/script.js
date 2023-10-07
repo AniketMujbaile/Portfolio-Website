fa9d99539fe88ccb85508f227dce058d73be8776
Aniket Mujbaile <aniketmujbaile1@gmail.com>
2023-10-07 09:35:12 +0530

    second commit

--- projects/script.js
+++ projects/script.js
@@ -110,7 +110,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/6520cbb06fcfe87d54b753c5/1hc41nb0o';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
